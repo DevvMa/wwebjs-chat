4b32855821fb26517ac5171385d384129023717f
Devva Maulana <maulanadevva.fitrah@gmail.com>
2024-05-18 13:03:31 +0700

    Change QR Terminal to QR display to browser and setting env

--- nodejs/wwebjs.js
+++ nodejs/wwebjs.js
@@ -1,7 +1,12 @@
 const { Client, LocalAuth } = require('whatsapp-web.js');
 const express = require('express');
-const qrcode = require('qrcode-terminal');
+const qrcode = require('qrcode');
 const cors = require('cors');
+
+require('dotenv').config();
+
+const HOST = process.env.HOST;
+const PORT = process.env.PORT;
 
 const app = express();
 app.use(express.json());
@@ -13,18 +18,55 @@ app.use(cors({
 }));
 
 const client = new Client({
-    authStrategy: new LocalAuth()
+    authStrategy: new LocalAuth(),
+    puppeteer: {
+        args: [
+            '--no-sandbox',
+            '--no-experiments',
+            '--hide-scrollbars',
+            '--disable-plugins',
+            '--disable-infobars',
+            '--disable-translate',
+            '--disable-pepper-3d',
+            '--disable-extensions',
+            '--disable-dev-shm-usage',
+            '--disable-notifications',
+            '--disable-setuid-sandbox',
+            '--disable-crash-reporter',
+            '--disable-smooth-scrolling',
+            '--disable-login-animations',
+            '--disable-dinosaur-easter-egg',
+            '--disable-accelerated-2d-canvas',
+            '--disable-rtc-smoothness-algorithm'
+        ],
+        headless: true
+    }
 });
 
-client.on('qr', qr => {
-    qrcode.generate(qr, {small: true});
-});
-
+// Notify client when device is successfully linked
 client.on('ready', () => {
-    console.log('Client is ready!');
+    res.json({ status: 'success', message: 'Device linked successfully' });
 });
 
+// Handle authentication failure
+client.on('auth_failure', () => {
+    res.status(500).json({ status: 'error', message: 'Failed to link device' });
+});
+
+// Initialize the client
 client.initialize();
+
+app.post('/generate-qr', (req, res) => {
+    // Generate and send QR code to client
+    client.on('qr', qr => {
+        qrcode.toDataURL(qr, (err, url) => {
+            if (err) {
+                return res.status(500).json({ status: 'error', message: 'Failed to generate QR code' });
+            }
+            res.json({ status: 'success', qrCode: url });
+        });
+    });
+});
 
 app.post('/send', (req, res) => {
     const number = req.body.number;
@@ -41,6 +83,6 @@ app.post('/send', (req, res) => {
     });
 });
 
-app.listen(3000, () => {
+app.listen(PORT, HOST, () => {
     console.log('Server is running on port 3000');
 });
